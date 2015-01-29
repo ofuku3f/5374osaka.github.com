fd31deb1c26ec61a1704a124d234a5371805c9a8
Fumiko Kurano <ofuku3f@gmail.com>
2015-01-30 02:55:23 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ var TrashModel = function(_lable, _cell, remarks) {
 
   for (var j in this.dayCell) {
     if (this.dayCell[j].length == 0) {
-      result_text += "この地域の収集はありません" + this.dayCell[j]; //収集のない町内は0としての対応 
+      result_text += "この地域の収集はありません"; //収集のない町内は0としての対応 
     } else if (this.dayCell[j].length == 1) {
       result_text += "毎週" + this.dayCell[j] + "曜日 ";
     } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) != "*") {
